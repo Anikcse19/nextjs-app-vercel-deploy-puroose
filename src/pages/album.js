92d56ef2deadcd47582c2e76a1580b5d7268bc01
Anik Chandra Deb <anikdebcse@gmail.com>
2023-08-24 20:00:10 +0600

    last commit

--- src/pages/album.js
+++ src/pages/album.js
@@ -5,10 +5,11 @@ const Album = () => {
   return (
     <div>
       <h1>Using img tag</h1>
-      <img
+      <Image
         src="https://gorzelinski.com/static/1db41e3ecd311724a15306b270d99dd9/6e87d/next-js-logo.png"
         alt=""
-        width="100%"
+         width={500}
+        height={500}
       />
 
       <h1>Using Image Component from Next.JS for Image optimizations</h1>
